04b3d4dcb8edb23a32e96cc7b36e1e80ade262ab
Siddarth Shantinath Patil <siddarth.patil@creditsafe.com>
2024-04-29 23:02:24 +0100

    Added DBv1 diagram, added scripts to alembic migration

--- alembic/versions/c74560a96592_initialize_database_schema.py
+++ alembic/versions/c74560a96592_initialize_database_schema.py
@@ -6,10 +6,16 @@ Create Date: 2024-04-26 22:01:37.653435
 
 """
 
+import uuid
 from typing import Sequence
 from typing import Union
 
 import sqlalchemy as sa
+from sqlalchemy import VARCHAR
+from sqlalchemy.types import UUID
+from sqlalchemy_utils import EmailType
+from sqlalchemy_utils import PasswordType
+from sqlalchemy_utils import TimezoneType
 
 from alembic import op
 
@@ -21,8 +27,65 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        "users",
+        sa.Column("user_id", UUID, primary_key=True, default=uuid.uuid4),
+        sa.Column("username", VARCHAR(128), nullable=False, unique=True),
+        sa.Column("email", EmailType, nullable=False, unique=True),
+        sa.Column("password", PasswordType, nullable=False),
+        sa.Column("created_at", TimezoneType, nullable=False),
+        sa.PrimaryKeyConstraint("user_id"),
+    )
+
+    op.create_table(
+        "groups",
+        sa.Column("group_id", UUID, primary_key=True, default=uuid.uuid4),
+        sa.Column("group_name", sa.String(), nullable=True),
+        sa.Column("created_by", sa.String(), nullable=True),
+        sa.Column("created_at", sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(["created_by"], ["users.user_id"]),
+        sa.PrimaryKeyConstraint("group_id"),
+    )
+
+    op.create_table(
+        "group_users",
+        sa.Column("group_id", sa.String(), nullable=False),
+        sa.Column("user_id", sa.String(), nullable=False),
+        sa.ForeignKeyConstraint(["group_id"], ["groups.group_id"]),
+        sa.ForeignKeyConstraint(["user_id"], ["users.user_id"]),
+        sa.PrimaryKeyConstraint("group_id", "user_id"),
+    )
+
+    op.create_table(
+        "expense",
+        sa.Column("expense_id", UUID, primary_key=True, default=uuid.uuid4),
+        sa.Column("user_id", sa.String(), nullable=True),
+        sa.Column("group_id", sa.String(), nullable=True),
+        sa.Column("amount", sa.Float(), nullable=True),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("created_at", sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(["group_id"], ["groups.group_id"]),
+        sa.ForeignKeyConstraint(["user_id"], ["users.user_id"]),
+        sa.PrimaryKeyConstraint("expense_id"),
+    )
+
+    op.create_table(
+        "split",
+        sa.Column("split_id", UUID, primary_key=True, default=uuid.uuid4),
+        sa.Column("expense_id", sa.String(), nullable=True),
+        sa.Column("user_id", sa.String(), nullable=True),
+        sa.Column("group_id", sa.String(), nullable=True),
+        sa.Column("amount", sa.Float(), nullable=True),
+        sa.ForeignKeyConstraint(["expense_id"], ["expense.expense_id"]),
+        sa.ForeignKeyConstraint(["group_id"], ["groups.group_id"]),
+        sa.ForeignKeyConstraint(["user_id"], ["users.user_id"]),
+        sa.PrimaryKeyConstraint("split_id"),
+    )
 
 
-def downgrade() -> None:
-    pass
+def downgrade():
+    op.drop_table("split")
+    op.drop_table("expense")
+    op.drop_table("group_users")
+    op.drop_table("groups")
+    op.drop_table("users")
